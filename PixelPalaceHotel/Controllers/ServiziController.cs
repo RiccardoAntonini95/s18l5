ea9775891e85d22dd3c0efb156c5603537fc36f4
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-03-08 15:26:34 +0100

    added form servizio aggiuntivo

--- PixelPalaceHotel/Controllers/ServiziController.cs
+++ PixelPalaceHotel/Controllers/ServiziController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PixelPalaceHotel.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +14,60 @@ namespace PixelPalaceHotel.Controllers
         // GET: Servizi
         public ActionResult NuovoServizio()
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["HotelDb"].ToString();
+            var conn = new SqlConnection(connectionString);
+            List<Prenotazione> listaPrenotati = new List<Prenotazione>();
+            conn.Open();
+            var commandList = new SqlCommand("SELECT * FROM Prenotazioni", conn);
+            var readerList = commandList.ExecuteReader();
+
+            if (readerList.HasRows)
+            {
+                while (readerList.Read())
+                {
+                    var prenotazione = new Prenotazione()
+                    {
+                        IdPrenotazione = (int)readerList["IdPrenotazione"],
+                        Cognome = (string)readerList["Cognome"],
+                        Nome = (string)readerList["Nome"]
+                    };
+                    listaPrenotati.Add(prenotazione);
+                }
+                ViewBag.listaPrenotati = listaPrenotati;
+                conn.Close();
+            }
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult NuovoServizio(Servizio nuovoServizio)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["HotelDb"].ToString();
+            var conn = new SqlConnection(connectionString);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    conn.Open();
+                    var command = new SqlCommand(@"INSERT INTO Servizi 
+                    (TipoServizio, DataServizio, Quantita, Prezzo, IdPrenotazione)
+                    VALUES (@tipoServizio, @dataServizio, @quantita, @prezzo, @idPrenotazione)", conn);
+                    command.Parameters.AddWithValue("@tipoServizio", nuovoServizio.TipoServizio);
+                    command.Parameters.AddWithValue("@dataServizio", nuovoServizio.DataServizio);
+                    command.Parameters.AddWithValue("@quantita", nuovoServizio.Quantita);
+                    command.Parameters.AddWithValue("@prezzo", nuovoServizio.Prezzo);
+                    command.Parameters.AddWithValue("@idPrenotazione", nuovoServizio.IdPrenotazione);
+                    var nRows = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    return View("error");
+                }
+                finally { conn.Close(); }
+                return RedirectToAction("LoggedIn", "Home");
+            }
+            return View();
+
         }
     }
 }
