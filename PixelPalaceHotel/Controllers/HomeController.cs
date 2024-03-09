67a33f80fc1f3bf022b6e8531870c433e3c54c8b
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-03-09 12:01:07 +0100

    query pensioni complete

--- PixelPalaceHotel/Controllers/HomeController.cs
+++ PixelPalaceHotel/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using PixelPalaceHotel.Models;
+﻿using Microsoft.Ajax.Utilities;
+using PixelPalaceHotel.Models;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -75,6 +76,65 @@ namespace PixelPalaceHotel.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetPrenotatiCodiceFiscale(string codiceFiscale)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["HotelDb"].ToString();
+            var conn = new SqlConnection(connectionString);
+            List<Prenotazione> listaPrenotati = new List<Prenotazione>();
+            try
+            {
+              conn.Open();
+              var commandList = new SqlCommand("SELECT * FROM Prenotazioni WHERE CodiceFiscale = @codiceFiscale", conn);
+              commandList.Parameters.AddWithValue("@codiceFiscale", codiceFiscale);
+              var readerList = commandList.ExecuteReader();
+              if (readerList.HasRows)
+              {
+                  while (readerList.Read())
+                  {
+                    var prenotazione = new Prenotazione()
+                    {
+                        IdPrenotazione = (int)readerList["IdPrenotazione"],
+                        CodiceFiscale = (string)readerList["CodiceFiscale"],
+                        Cognome = (string)readerList["Cognome"],
+                        Nome = (string)readerList["Nome"],
+                        Citta = (string)readerList["Citta"],
+                        InizioSoggiorno = (DateTime)readerList["InizioSoggiorno"],
+                        FineSoggiorno = (DateTime)readerList["FineSoggiorno"]                     
+                     };
+                     listaPrenotati.Add(prenotazione);
+                  }
+              }
+
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+            finally { conn.Close(); }
+            return Json(listaPrenotati, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetTotalePensioneCompleta()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["HotelDb"].ToString();
+            var conn = new SqlConnection(connectionString);
+            int totalePensioniComplete = 0;
+            try
+            {
+                conn.Open();
+                var commandList = new SqlCommand("SELECT COUNT (*) FROM Prenotazioni WHERE TipoSoggiorno = 'Pensione completa'", conn);
+                totalePensioniComplete = (int)commandList.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+            finally { conn.Close(); }
+            return Json(totalePensioniComplete, JsonRequestBehavior.AllowGet);
         }
     }
 }
